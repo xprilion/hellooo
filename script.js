e7bab14d245383a248c0dc62bddfd233cbc10cb5
Anubhav Singh <xprilion@gmail.com>
2023-12-19 15:09:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var greeting = document.getElementById("greeting");
+    var count = sessionStorage.getItem("count") || 1;
+
+    greeting.textContent += "o".repeat(count);
+    sessionStorage.setItem("count", count + 1);
+});
